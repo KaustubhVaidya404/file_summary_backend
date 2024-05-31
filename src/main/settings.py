ca2f604107cf39d46367b0565d79b5dca247c380
Kaustubh Anirudha Vaidya <140918319+KaustubhVaidya404@users.noreply.github.com>
2024-05-31 08:40:13 +0530

    vercel deployment configs

--- src/main/settings.py
+++ src/main/settings.py
@@ -27,9 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-6a@44o^2v8x#z%+=$+a&qup8bta0u0sq=x$u49n0z@1&yfphwh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
